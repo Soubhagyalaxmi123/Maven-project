9ba1eafaafb6767fb15c783f6f844f1bf2c6ce0a
Soubhagyalaxmi123 <soubhagyalaxmimaharana84@gmail.com>
2023-05-22 22:31:24 +0530

    commit maven project

---
+++ src/test/java/com/package2/Login.java
@@ -0,0 +1,24 @@
+package com.package2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+  @Test
+  public void login_test() {
+	
+	  WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.close();
+  }
+  
+}
